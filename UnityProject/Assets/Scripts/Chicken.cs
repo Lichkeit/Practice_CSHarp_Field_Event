d9155d5b694dbe7c8ff6e752c63a567a0b5598d9
Lichkeit <57181190+Lichkeit@users.noreply.github.com>
2019-11-12 21:34:49 +0800

    功課2 欄位與事件

---
+++ UnityProject/Assets/Scripts/Chicken.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+
+    [Header("移動速度"),Range(0,9999f)]
+	public float   speed = 1f  ;
+
+	[Header("跳要高度"),Range(0,9999)]
+	public int   Jump = 1  ;
+
+	[Header("對話內容")]
+	 public  string  Talk  ;
+
+
+	 [Header("是否取的雞蛋")]
+	 public  bool  Egg  ;
+
+    void Start()
+    {
+        print("遊戲開始");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       print(Talk);
+        
+    }
+}
